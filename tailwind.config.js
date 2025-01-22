6c6ea9b4eceb7b47ebeaecb1bd74090c2bf642a7
Lucas Santos <142832494+desenvolvedor13@users.noreply.github.com>
2025-01-22 15:53:58 -0300

    Página index (git na master)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          100: '#F1C27D',
+          200: '#D8A93E',
+          600: '#B88A2F',
+          700: '#8B6F2E',
+          800: '#7A5A2D',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
